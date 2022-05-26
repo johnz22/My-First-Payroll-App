c7b99634aefe0bfadcc246315dbcad46b873eb8b
DotNetCore <36761115+johnz22@users.noreply.github.com>
2022-05-27 01:04:38 +1000

    Add project files.

---
+++ Payroll/PayrollMethods.cs
@@ -0,0 +1,58 @@
+﻿namespace ClassDemo
+{
+    internal class PayrollMethods
+    {
+        private const int hourlyRate = 27;
+        private int _hoursWorked;
+        private string? nameOfStaff;
+
+        public int HoursWorked
+        {
+            get
+            {
+                return _hoursWorked;
+            }
+            set
+            {
+                if (value > 0)
+                    _hoursWorked = value;
+                else
+                    _hoursWorked = 0;
+            }
+        }
+        public string NameOfStaff
+        {
+            get { return nameOfStaff; }
+            set { nameOfStaff = value; }
+        }
+        public int CalculatePay()
+        {
+            PrintMessage();
+            int staffPay;
+            staffPay = HoursWorked * hourlyRate;
+            if (HoursWorked > 0)
+                return staffPay;
+            else
+                return 0;
+        }
+
+        public int CalculatePay(int bonus, int allowance)
+        {
+            PrintMessage();
+            if (HoursWorked > 0)
+                return HoursWorked * hourlyRate + bonus + allowance;
+            else
+                return 0;
+        }
+
+        public void PrintMessage()
+        {
+            Console.WriteLine("Calculating Pay…………");
+        }
+
+        public override string ToString()
+        {
+            return "Name of Staff = " + NameOfStaff + ", hourlyRate = " + hourlyRate + ", HoursWorked = " + HoursWorked;
+        }
+    }
+}
---
+++ Payroll/Program.cs
@@ -0,0 +1,41 @@
+﻿
+namespace ClassDemo
+{
+    class Staff : PayrollMethods
+    {
+        public Staff(string name)
+        {
+            NameOfStaff = name;
+            Console.WriteLine("\n" + NameOfStaff);
+            Console.WriteLine("--------------------------");
+        }
+
+        public Staff(string firstName, string lastName)
+        {
+            NameOfStaff = firstName + " " + lastName;
+            Console.WriteLine("\n" + NameOfStaff);
+            Console.WriteLine("--------------------------");
+        }
+
+        static void Main()
+        {
+            int pay;
+            var staff1 = new Staff("John");
+            staff1.HoursWorked = 160;
+            pay = staff1.CalculatePay(1000, 400);
+            Console.WriteLine("Pay = {0}", pay);
+
+            Staff staff2 = new Staff("Mark", "Hong");
+            staff2.HoursWorked = 120;
+            pay = staff2.CalculatePay();
+            Console.WriteLine("Pay = {0}", pay);
+
+            Staff staff3 = new Staff("Jackie");
+            staff3.HoursWorked = -10;
+            pay = staff3.CalculatePay();
+            Console.WriteLine("Pay = {0}", pay);
+        }
+
+    }
+}
+
